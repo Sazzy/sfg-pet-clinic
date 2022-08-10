8d70619b23e12f78a1b1ebf1ee2d2fa7de21c283
Eugen Sazonov <saz.vit@gmail.com>
2022-08-11 00:14:38 +0200

    Added profiles for Map Services. Tested profiles turning on. Closes #50.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/SpecialtyTypeMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/SpecialtyTypeMapService.java
@@ -2,11 +2,13 @@ package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.SpecialtyType;
 import guru.springframework.sfgpetclinic.services.SpecialtyTypeService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class SpecialtyTypeMapService extends AbstractMapService<SpecialtyType, Long> implements SpecialtyTypeService {
 	@Override
 	public Set<SpecialtyType> findAll() {
